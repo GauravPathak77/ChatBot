ddaf8774e5960d07dc13278a4aee77be30cc024e
GauravPathak77 <115170827+GauravPathak77@users.noreply.github.com>
2023-08-06 22:35:54 +0530

    ChatBot V1.1

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ require("dotenv").config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index", {response: null});
